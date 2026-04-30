e9e3b98149c2234d95ed659a18d8a6fe019e5c22
gabristavares <soarestavaresgabriel@gmail.com>
2026-04-29 21:02:15 -0300

    apostila8

---
+++ Apostila08/src/br/com/fiap/apostila08/view/ex1apt8.java
@@ -0,0 +1,13 @@
+package br.com.fiap.apostila08.view;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class ex1apt8 {
+    public static void main(String[] args) {
+
+        String palavra = JOptionPane.showInputDialog("Digite uma palavra:");
+        System.out.println(palavra.toUpperCase());
+
+    }
+}
